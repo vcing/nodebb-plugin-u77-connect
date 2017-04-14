4a5db8fdca52257bdb81895b27ced1e7410e3b13
vcing <2131287@qq.com>
2017-04-14 16:06:27 +0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 require("babel-polyfill");
 
-export default class u77Connect {
+class U77Connect {
     init(data,callback) {
         console.log('---------------u77 connect----------------');
         console.log(data);
     }
 }
+let u77Connect = new U77Connect();
+export default u77Connect
