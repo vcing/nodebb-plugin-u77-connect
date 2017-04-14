20b0721c31e2b7b588c11feba84b9d9f22855bae
vcing <2131287@qq.com>
2017-04-14 15:49:40 +0800

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+require("babel-polyfill");
+
+export default class u77Connect {
+    init(data,callback) {
+        console.log('---------------u77 connect----------------');
+        console.log(data);
+    }
+}
