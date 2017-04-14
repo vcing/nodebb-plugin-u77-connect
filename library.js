bf20b388126fb6decc2b8ed11c2395754d99eb86
vcing <2131287@qq.com>
2017-04-14 16:41:49 +0800

    update

---
+++ library.js
@@ -0,0 +1,6 @@
+var U77Connect = {};
+U77Connect.init = function(data,callback) {
+    console.log('-----------------u77 connect-----------------');
+    callback();
+}
+module.exports = U77Connect;
