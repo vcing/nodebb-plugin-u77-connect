11de224dff624f17c06e7e383b669c01fae23b01
vcing <2131287@qq.com>
2017-04-18 10:07:53 +0800

    update overrideLogin interface

--- library.js
+++ library.js
@@ -1,6 +1,28 @@
-var U77Connect = {};
-U77Connect.init = function(data,callback) {
-    console.log('-----------------u77 connect-----------------');
-    callback();
-}
-module.exports = U77Connect;
+(function (module) {
+    "use strict";
+    var passport = module
+        .parent
+        .require("passport");
+    var passportLocal = module
+        .parent
+        .require('passport-local')
+        .Strategy;
+    var U77Connect = {};
+
+    U77Connect.init = function (data, callback) {
+        console.log('-----------------u77 connect-----------------');
+        callback();
+    }
+
+    U77Connect.auth = function () {
+        passport.use(new passportLocal({
+            passReqToCallback: true
+        }, login));
+    }
+
+    function login(req, username, password, next) {
+        next(null,{uid:1});
+    }
+
+    module.exports = U77Connect;
+}(module));
