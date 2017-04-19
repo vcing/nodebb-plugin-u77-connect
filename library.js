f75de10c2b7def0ddfbaf62039fc4f1dd84a91fc
vcing <2131287@qq.com>
2017-04-19 10:11:39 +0800

    finish username/password login

--- library.js
+++ library.js
@@ -8,8 +8,16 @@
         .require('passport-local')
         .Strategy;
     var U77Connect = {};
-    var controllers = module.parent.require('./controllers');
-
+    var controllers = module
+        .parent
+        .require('./controllers');
+    var User = module
+        .parent
+        .require('./user');
+    var db = module
+        .parent
+        .require('./database');
+    var request = require('request');
 
     U77Connect.init = function (params, callback) {
         console.log('-----------------u77 connect-----------------');
@@ -23,11 +31,54 @@
     }
 
     function login(req, username, password, next) {
-        next(null, {uid: 1});
+        request
+            .post('http://n.u77.com/member/login-api', {
+                form: {
+                    username: username,
+                    password: password
+                }
+            }, function (err, response, body) {
+                try {
+                    body = JSON.parse(body);
+                    body.user.detail = JSON.parse(body.user.detail);
+                } catch (e) {
+                    next(e);
+                    return;
+                }
+                if (err) {
+                    next(err);
+                    return;
+                }
+                if (body.err) {
+                    next(null, false, body.msg);
+                    return;
+                }
+                if (body.user.detail.uid) {
+                    next(null, {uid: body.user.detail.uid});
+                    return;
+                }
+                let userData = {
+                    username: body.user.name,
+                    email: body.user.email || '',
+                    password: (Math.random() * 10000000000).toFixed(0),
+                    picture: 'http://img.u77.com/avatar/' + body.user.avatar,
+                    gravatarpicture: 'http://img.u77.com/avatar/' + body.user.avatar
+                }
+                User.create(userData, function (err, uid) {
+                    if (err) {
+                        next(err);
+                        return;
+                    }
+                    request('http://n.u77.com/user/uid?id=' + body.user.id + '&uid=' + uid, function (err, response, body) {
+                        next(null, {uid: uid});
+                    });
+                });
+            });
     }
 
-    U77Connect.routeFilter = function(req,res,next) {
+    U77Connect.routeFilter = function (req, res, next) {
         // controllers.register(req,res,next);
+        next();
     }
 
     module.exports = U77Connect;
